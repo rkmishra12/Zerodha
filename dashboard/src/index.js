5f9b51663f7405944adace8159669160abe69e5f
Ratikanta <ratikantamishra16@gmail.com>
2025-07-23 20:19:56 +0530

    Deployed link replacing

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -15,7 +15,7 @@ root.render(
         <Routes>
           <Route path="/" element={<Login />} />
           <Route
-            path="/*"
+            path="/dashboard"
             element={
               <ProtectedRoute>
                 <Home />
