0e0b3bba59a3a3f06c573fb1d87fe552be123e04
Ratikanta <ratikantamishra16@gmail.com>
2025-07-27 22:07:12 +0530

    PORT change

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 const WatchlistModel = require("./model/WatchlistModel");
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 const uri = process.env.MONGO_URL;
 
 const app = express();
